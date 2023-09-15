6b220434f72b5ee759877c41ed2d1fdd8dead586
I550440 <marcel.bulling@sap.com>
2023-09-15 09:43:26 +0200

    page init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 <script>
-	
+
 </script>
 
 <main>
-
+	<a href="/accounts">Accounts</a><br>
+	<a href="/statistiks">Statistik</a>
 </main>
 
 <style lang="postcss">
