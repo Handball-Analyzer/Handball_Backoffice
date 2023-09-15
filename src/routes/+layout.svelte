aecfd1ea1fcd15f46594fb864d756a017289d21f
I550440 <marcel.bulling@sap.com>
2023-09-15 09:32:54 +0200

    final app_init

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,44 +6,20 @@
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
+	import Navbar from '../components/Navbar.svelte';
 </script>
 
 <!-- App Shell -->
+
 <AppShell>
-	<svelte:fragment slot="header">
-		<!-- App Bar -->
-		<AppBar>
-			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
-			</svelte:fragment>
-			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
-			</svelte:fragment>
-		</AppBar>
-	</svelte:fragment>
-	<!-- Page Route Content -->
+	<Navbar/>
+	<!--<svelte:fragment slot="sidebarLeft">Sidebar Left</svelte:fragment> -->
+	<!-- (sidebarRight) -->
+	<!-- (pageHeader) -->
+	<!-- Router Slot -->
 	<slot />
+	<!-- ---- / ---- -->
+	<!-- (pageFooter) -->
+	<!-- <svelte:fragment slot="footer">Footer</svelte:fragment> -->
 </AppShell>
